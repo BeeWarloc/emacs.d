af2a48e10e46c4ab8d38fc609ca178d6635c673a
Karsten Strand <karstens@graphcore.ai>
2018-10-19 18:35:18 +0200

    Enable mouse in terminal

--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (smex color-theme-sanityinc-tomorrow use-package))))
+ '(mouse-yank-at-point t)
+ '(package-selected-packages (quote (use-package)))
+ '(xterm-mouse-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
