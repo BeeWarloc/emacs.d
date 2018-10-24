c970cf935d0941ca41158471ceb10e2158708155
Karsten Nikolai Strand <karsten.strand@gmail.com>
2018-10-24 13:35:17 +0200

    More ido plus which-key

--- custom.el
+++ custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(mouse-yank-at-point t)
- '(package-selected-packages (quote (crux use-package)))
+ '(package-selected-packages
+   (quote
+    (which-key ido-describe-bindings ido-describe-prefix-bindings ido-grid-mode use-package)))
  '(xterm-mouse-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
