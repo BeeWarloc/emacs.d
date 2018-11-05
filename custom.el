c2501e916ed8779231f5f039cd851a37be325105
Karsten Nikolai Strand <karsten.strand@gmail.com>
2018-11-05 19:07:16 +0100

    .editorconfig support

--- custom.el
+++ custom.el
@@ -4,9 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(mouse-yank-at-point t)
- '(package-selected-packages
-   (quote
-    (magit which-key ido-describe-bindings ido-describe-prefix-bindings ido-grid-mode use-package)))
+ '(package-selected-packages (quote (use-package)))
  '(xterm-mouse-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
